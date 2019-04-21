a89b5aad23fa858ed202b0f27c7d3d5d2b0149a3
sherruh <sherruh@gmail.com>
2019-04-21 18:04:16 +0600

    add tablayout

--- app/src/main/java/com/geektech/lastfmapp/presentation/main/MainActivity.java
+++ app/src/main/java/com/geektech/lastfmapp/presentation/main/MainActivity.java
@@ -1,5 +1,11 @@
 package com.geektech.lastfmapp.presentation.main;
 
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +17,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        viewPager.setAdapter(
+                new SampleFragmentPagerAdapter(getSupportFragmentManager(), MainActivity.this));
+
+        TabLayout tabLayout = findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+        final int PAGE_COUNT = 2;
+        private String tabTitles[] = new String[] { "Top tracks", "Top artists"};
+
+
+        public SampleFragmentPagerAdapter(FragmentManager fm, Context context) {
+            super(fm);
+        }
+
+        @Override public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        @Override public Fragment getItem(int position) {
+            return PageFragment.newInstance(position + 1);
+        }
+
+        @Override public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
     }
 }
